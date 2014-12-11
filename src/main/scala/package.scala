9c900801e418b27e88caaf4671e62c00a5d99632
ischneider <ischneider@localhost>
2014-12-11 17:05:48 -0600

    Scores implemented

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -4,4 +4,8 @@ package edu.luc.etl.cs313.scala
 package object uidemo {
   val TAG = "edu.luc.etl.cs313.scala.uidemo" // for logging
   val DOT_DIAMETER = 40 // TODO externalize
+
+  val MonstersQuantity = Array(5, 6, 7, 8, 9, 10, 12, 15)
+  val ChangeInterval = Array(10000, 8000, 6000, 4000, 2000, 1000, 500, 250)
+  val MoveInterval = Array(8000, 6000, 5000, 4000, 3000, 2000, 1000, 500)
 }
